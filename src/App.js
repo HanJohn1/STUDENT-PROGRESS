74197a5a2859b2832b4edd565c3305b3d64b6c7e
HanJohn1 <-->
2025-02-27 08:43:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./LoginPage";  
-import StudentProgressTracker from "./StudentProgressTracker";  
+import { Routes, Route, Link } from "react-router-dom";
+import StudentProgressTracker from "./StudentProgressTracker";
+import LoginPage from "./LoginPage";
 
 function App() {
   return (
-    <Router>
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Student Progress Tracker</h1>
+
+      {/* Navigation Links */}
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/tracker">Tracker</Link> | <Link to="/login">Login</Link>
+      </nav>
+
+      {/* Routes */}
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<h2>Welcome to the app</h2>} />
         <Route path="/tracker" element={<StudentProgressTracker />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
